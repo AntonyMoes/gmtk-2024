0ec0a0e65a4b3b1b46e2b21f713b55832ec1f8a4
AntonyMo <32247865+AntonyMoes@users.noreply.github.com>
2024-08-18 17:03:10 +0400

    Implement checkpoints

---
+++ Assets/_Game/Scripts/Checkpoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using GeneralUtils;
+using UnityEngine;
+
+namespace _Game.Scripts {
+    public class Checkpoint : MonoBehaviour {
+        [SerializeField] private Transform _spawn;
+        public Transform Spawn => _spawn;
+
+        private readonly Event<Checkpoint> _onEnter = new Event<Checkpoint>();
+        public IEvent<Checkpoint> OnEnter => _onEnter;
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.GetComponent<PlayerController>() != null) {
+                _onEnter?.Invoke(this);
+            }
+        }
+    }
+}
