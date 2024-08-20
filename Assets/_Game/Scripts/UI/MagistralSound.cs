d9bfbcf8fbdd65121c87457324fedfb2c506a7c9
darkenezy <d4rkenezy@gmail.com>
2024-08-20 06:08:17 +0300

    less bullshit code

--- Assets/_Game/Scripts/UI/MagistralSound.cs
+++ Assets/_Game/Scripts/UI/MagistralSound.cs
@@ -1,28 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 namespace _Game.Scripts.UI {
     public class MagistralSound : MonoBehaviour
     {
         void Start()
         {
-            
+            InvokeRepeating("DoBeep", 0f, 20f);
         }
 
-        void Update()
+        void DoBeep()
         {
+            Debug.Log("BEEP");
             var playerPosition = FindObjectOfType<PlayerController>().gameObject.transform.position;
             var distanceToPlayer = (playerPosition - transform.position).magnitude;
-            if (distanceToPlayer > 150f) {
+            Debug.Log(distanceToPlayer);
+            if (distanceToPlayer > 250f) {
                 return;
             }
             
-            if (Random.value < 0.0005f) {
-                var soundName = Random.value < 0.66f ? "car": "truck";
-                var soundVolume = Mathf.Exp(0.06f - 0.03f * distanceToPlayer);
-                SoundController.Instance.PlaySound(soundName, soundVolume, Random.Range(0.5f, 1.5f));
-            }
+            var soundName = Random.value < 0.66f ? "car": "truck";
+            var soundVolume = Mathf.Exp(0.06f - 0.03f * distanceToPlayer);
+
+            DOTween.Sequence()
+                .AppendInterval(Random.Range(0f, 10f))
+                .AppendCallback(
+                    () => {
+                        SoundController.Instance.PlaySound(soundName, soundVolume, Random.Range(0.5f, 1.5f));
+                    }
+                );
+            
         }
     }
 }
