36a12f31066bb13e6c6825eaaaa65008eb118a2a
AntonyMo <32247865+AntonyMoes@users.noreply.github.com>
2024-08-20 13:18:56 +0400

    Fix MagistralSound

--- Assets/_Game/Scripts/UI/MagistralSound.cs
+++ Assets/_Game/Scripts/UI/MagistralSound.cs
@@ -1,36 +1,59 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using Random = UnityEngine.Random;
 
 namespace _Game.Scripts.UI {
-    public class MagistralSound : MonoBehaviour
-    {
-        void Start()
-        {
-            InvokeRepeating("DoBeep", 0f, 20f);
+    public class MagistralSound : MonoBehaviour {
+        private Coroutine _beepRoutine;
+        private Tween _currentAnimation;
+        private PlayerController _player;
+
+        private void Start() {
+            _beepRoutine = StartCoroutine(BeepRoutine());
         }
 
-        void DoBeep()
-        {
-            var playerPosition = FindObjectOfType<PlayerController>().gameObject.transform.position;
+        private void OnDestroy() {
+            if (_beepRoutine != null) {
+                StopCoroutine(_beepRoutine);
+                _beepRoutine = null;
+            }
+
+            _currentAnimation?.Kill();
+            _currentAnimation = null;
+        }
+
+        private IEnumerator BeepRoutine() {
+            while (true) {
+                DoBeep();
+                yield return new WaitForSeconds(20f);
+            }
+        }
+
+        private void DoBeep() {
+            if (_player == null) {
+                _player = FindObjectOfType<PlayerController>();
+                if (_player == null) {
+                    return;
+                }
+            }
+
+            var playerPosition = _player.transform.position;
             var distanceToPlayer = (playerPosition - transform.position).magnitude;
 
             if (distanceToPlayer > 250f) {
                 return;
             }
-            
-            var soundName = Random.value < 0.66f ? "car": "truck";
+
+            var soundName = Random.value < 0.66f ? "car" : "truck";
             var soundVolume = Mathf.Exp(0.06f - 0.03f * distanceToPlayer);
 
-            DOTween.Sequence()
-                .AppendInterval(Random.Range(0f, 10f))
-                .AppendCallback(
-                    () => {
-                        SoundController.Instance.PlaySound(soundName, soundVolume, Random.Range(0.5f, 1.5f));
-                    }
-                );
-            
+            _currentAnimation = DOTween.Sequence()
+                .AppendInterval(Random.Range(0.2f, 10f))
+                .AppendCallback(() => {
+                    SoundController.Instance.PlaySound(soundName, soundVolume, Random.Range(0.5f, 1.5f));
+                    _currentAnimation = null;
+                });
         }
     }
 }
